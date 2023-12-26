9af04140f8a1a70bb5af49185f51c11ec8909226
Atn71 <82682016+Atn71@users.noreply.github.com>
2023-12-26 20:25:10 +0300

    Update marzban-to-mysql.sh

--- marzban-to-mysql.sh
+++ marzban-to-mysql.sh
@@ -159,7 +159,7 @@ ${no_color}"
 }
 
 check_server_os() {
-  if [ "$OS_NAME" == "Ubuntu" ] && [ "$OS_VERSION" == "22.04" ] || [ "$OS_VERSION" == "20.04" ]; then
+  if [ "$OS_NAME" == "" ] && [ "$OS_VERSION" == "" ] || [ "$OS_VERSION" == "" ]; then
     echo -e "${green}${T[101]}${OS_VERSION}${no_color}"
   else
     echo -e "${red}${T[000]} ${T[100]}${no_color}" && exit 1
@@ -168,7 +168,7 @@ check_server_os() {
 
 install_pkgs() {
 
-  if [[ $(lsb_release -rs) == "20.04" ]]; then
+  if [[ $(lsb_release -rs) == "" ]]; then
 
     echo -e "\n ${blue} sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser ${no_color} \n"
     sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser
@@ -423,7 +423,7 @@ installing_phpmyadmin() {
   if [ "$phpmyadmin_installation_response" == "Y" ] || [ "$phpmyadmin_installation_response" == "y" ] || [ -z "$phpmyadmin_installation_response" ]; then
     echo -e "\n ${blue}editing docker-compose.yml to add phpmyadmin${no_color}\n"
     phpmyadmin_service_line=$(grep -n "phpmyadmin:" "$docker_compose_file_path" | head -n 1 | cut -d: -f1)
-    phpmyadmin_service=" \ \ phpmyadmin:\n    image: phpmyadmin\/phpmyadmin:latest\n    restart: always\n    env_file: .env\n    network_mode: host\n    environment:\n      PMA_HOST: 127.0.0.1\n      APACHE_PORT: 8010\n      UPLOAD_LIMIT: 1024M\n    depends_on:\n      - mysql\n\n"
+    phpmyadmin_service=" \ \ phpmyadmin:\n    image: phpmyadmin\/phpmyadmin:latest\n    restart: always\n    env_file: .env\n    network_mode: host\n    environment:\n      PMA_HOST: 127.0.0.1\n      APACHE_PORT: 7409\n      UPLOAD_LIMIT: 1024M\n    depends_on:\n      - mysql\n\n"
     if [ -z "$phpmyadmin_service_line" ]; then
        
        insert_line=$((volumes_line + 1))
